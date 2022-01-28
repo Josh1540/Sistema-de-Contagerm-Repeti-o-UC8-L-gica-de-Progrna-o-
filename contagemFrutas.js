da1aa6f40cad268456f2814a02dd89547028e222
Joshua <joshuaamaral05@gmail.com>
2022-01-28 19:09:25 -0300

    Sistema de Contagem- N1-10__UC8 Lógica de Programação

---
+++ contagemFrutas.js
@@ -0,0 +1,6 @@
+let listaSabores = ["Morango","Melancia","Laranja"]
+
+for(let contador = 0;contador < 3;contador++){
+
+    console.log (`Temos os seguintes sabores ${listaSabores[contador]}`)
+}
---
+++ contagemNúmeros.js
@@ -0,0 +1,13 @@
+let numeroAlunos = 10
+
+for (let contador = 0; contador < numeroAlunos ; contador++){
+    //Resto % -> Retorna o valor que SOBRA-RESTA DA DIVISÃO
+if (contador == 0 ) {
+        console.log ("O número é ZERO")
+} else if ( contador % 2 == 0) {
+    console.log (`O Número ${contador} é PAR`)
+
+} else if (contador % 2 == 1 ){
+
+    console.log (`O Número ${contador} é IMPAR`)}
+}
